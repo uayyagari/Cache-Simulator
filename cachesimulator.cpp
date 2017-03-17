f2b45b98597eb2a4ad4bd8e3eb1e5834196a063d
Uday Ayyagari <ua337@nyu.edu>
2017-03-17 02:37:38 -0400

    editing comments

--- cachesimulator.cpp
+++ cachesimulator.cpp
@@ -34,46 +34,32 @@ struct config{
        int L2size;
        };
 
-class check_cache
-{
+class check_cache{
 public: int s,b,t;
-    // cache_struct sa[];
-    
-    
-    check_cache( int blocksize ,int setsize ,int size)
-    {
-        //cache_struct dm;
-        //cache r;
-        //int s,b,t;
-        if (setsize == 0)
-        {
-            cout<<"full asso\n"<<endl;        // still have to implement
+        
+    check_cache( int blocksize ,int setsize ,int size){
+        
+        if (setsize == 0){
+            cout<<"full asso\n"<<endl;        
             b = log2(blocksize);
             cout<<b<<"  offset\n"<<endl;
-            s = log2(size) + 10 - log2(blocksize) ;    // removed set size for further modification check if it is needed
+            s = log2(size) + 10 - log2(blocksize) ;    
             cout<<s<<" index \n"<<endl;
             t= 32-s-b;
             cout<<t<<" Tag\n"<<endl;
         }
-        else if ( setsize == 1)
-        {
+        else if ( setsize == 1){
             
             cout<<"Direct mapped\n"<<endl;
             b = log2(blocksize);
             cout<<b<<"  offset\n"<<endl;
-            s = log2(size) + 10 - log2(blocksize) - log2(setsize); //remove set size
+            s = log2(size) + 10 - log2(blocksize) - log2(setsize); 
             cout<<s<<" index \n"<<endl;
             t= 32-s-b;
             cout<<t<<" Tag\n"<<endl;
-            //cache_struct dm;
-            //dm.cache_struct1(b, s);
-            //sa.cache_struct1(b,s);
-            //double data_array[(int)pow(2,s)][b];
-            //cout<<(int)pow(2,s)<<"\n"<<endl;
-            
+           
         }
-        else if (log2(setsize)==floor(log2(setsize)))    //check    log2(setsize)==floor(log2(setsize))    Or setsize%2==0
-        {
+        else if (log2(setsize)==floor(log2(setsize))){
             cout<<"Set Associativity\n"<<endl;
             b = log2(blocksize);
             cout<<b<<"  offset\n"<<endl;
@@ -81,12 +67,6 @@ public: int s,b,t;
             cout<<s<<" index \n"<<endl;
             t= 32-s-b;
             cout<<t<<" Tag\n"<<endl;
-            //cache_struct sa[setsize];           //out of bounds?
-            
-            // int tag_array[(int)pow(2,s)];          //check the data type boolean ? / int ?
-            // bool valid_array[(int)pow(2,s)];
-            
-            //error debugginh
         }
     }
 };
@@ -99,8 +79,8 @@ int main(int argc, char* argv[]){
     ifstream cache_params;
     string dummyLine;
     cache_params.open(argv[1]);
-    while(!cache_params.eof())  // read config file
-    {
+    
+    while(!cache_params.eof()){
       cache_params>>dummyLine;
       cache_params>>cacheconfig.L1blocksize;
       cache_params>>cacheconfig.L1setsize;              
@@ -109,13 +89,8 @@ int main(int argc, char* argv[]){
       cache_params>>cacheconfig.L2blocksize;           
       cache_params>>cacheconfig.L2setsize;        
       cache_params>>cacheconfig.L2size;
-      }
-    
-  
-  
-   // Implement by you: 
-   // initialize the hirearch cache system with those configs
-   // probably you may define a Cache class for L1 and L2, or any data structure you like
+    }
+
     check_cache mycacheL1( cacheconfig.L1blocksize , cacheconfig.L1setsize , cacheconfig.L1size);
     check_cache mycacheL2( cacheconfig.L2blocksize , cacheconfig.L2setsize , cacheconfig.L2size);
     int pL1 = pow(2, mycacheL1.s);
@@ -127,14 +102,9 @@ int main(int argc, char* argv[]){
     
     int valid_arrayL1[pL1 - 1][cacheconfig.L1setsize];
     int valid_arrayL2[qL2-1][cacheconfig.L2setsize];
-    
-    
    
-   
-   
-  int L1AcceState =0; // L1 access state variable, can be one of NA, RH, RM, WH, WM;
-  int L2AcceState =0; // L2 access state variable, can be one of NA, RH, RM, WH, WM;
-   
+    int L1AcceState =0; 
+    int L2AcceState =0; 
    
     ifstream traces;
     ofstream tracesout;
@@ -158,189 +128,143 @@ int main(int argc, char* argv[]){
             stringstream saddr(xaddr);
             saddr >> std::hex >> addr;
             accessaddr = bitset<32> (addr);
-                                                // access the L1 and L2 Cache according to the trace;
-                                                // cout<<accessaddr << "Address in binary\n";
-                                                //cout<<xaddr << "Address in hex\n";
+                                                
             int countL1 = 0 ;
             int countL2 = 0;
             int ta1 = mycacheL1.t;
             int ind1 = mycacheL1.s;
-            int off1 = mycacheL1.b;  // new
+            int off1 = mycacheL1.b; 
             cout<<"this is the address in hex"<< xaddr <<"\n"<<endl;
             cout<<"this is the access address in hex"<< accessaddr <<"\n"<<endl;
             
-            //bitset<32> tagl1 ((accessaddr.to_string().substr(0,ta1)));
             int tagL1 = (bitset<32>((accessaddr.to_string().substr(0,ta1))).to_ulong());
             cout<<tagL1<<"this is tag1 x in main for L1"<<endl;
             
-            //bitset<32> indexl1 ((accessaddr.to_string().substr(ta1,ind1)));
             int indexL1 = (bitset<32>((accessaddr.to_string().substr(ta1,ind1))).to_ulong());
             cout<<indexL1<<"this is index1 x in main for L1"<<endl;
             
-            //bitset<32> offl1 ((accessaddr.to_string().substr(ta1+ind1,off1)));
             int offL1 =(bitset<32>((accessaddr.to_string().substr(ta1+ind1,off1))).to_ulong());
             cout<<offL1<<"this is offset1 x in main for L1"<<endl;
             
             
             int ta2 = mycacheL2.t;
             int ind2 = mycacheL2.s;
-            int off2=mycacheL2.b;  // new
+            int off2=mycacheL2.b; 
+            
             cout<<"this is the address in hex"<< xaddr <<"\n"<<endl;
             cout<<"this is the access address in hex"<< accessaddr <<"\n"<<endl;
             
-            //bitset<32> tagl2 ((accessaddr.to_string().substr(0,ta2)));
             int tagL2 =  (bitset<32>((accessaddr.to_string().substr(0,ta2))).to_ulong());
             cout<<tagL2<<"this is tag2 x in main for L2"<<endl;
             
-            //bitset<32> indexl2 ((accessaddr.to_string().substr(ta2,ind2)));
             int indexL2 (bitset<32>((accessaddr.to_string().substr(ta2,ind2))).to_ulong());
             cout<<indexL2<<"this is index2 x in main for L2"<<endl;
             
-            //bitset<32> offl2(( accessaddr.to_string().substr(ta2+ind2,off2)));
-             int offL2 (bitset<32>((accessaddr.to_string().substr(ta2+ind2,off2))).to_ulong());
+            int offL2 (bitset<32>((accessaddr.to_string().substr(ta2+ind2,off2))).to_ulong());
             cout<<offL2<<"this is offset2 x in main for L2"<<endl;
-            //
-            if (accesstype.compare("R")==0)
-            {
-                 //Implement by you:
+        
+            if (accesstype.compare("R")==0){
                  // read access to the L1 Cache, 
                  //  and then L2 (if required), 
                  //  update the L1 and L2 access state variable;
-                 for(int jj = 0 ; jj<cacheconfig.L1setsize;jj++)
-                 {
-                     if((tag_arrayL1[indexL1][jj] == tagL1) && (valid_arrayL1[indexL1][jj] == 1))
-                        {
-                            L2AcceState =0;
-                            L1AcceState =1;
-                            cout<<" Read HIT NA \n";
-                            break;
-                        }
-                    }
-                  if(L1AcceState == 0)
-                        {
-                            L1AcceState = 2;
-                            for(int jj = 0;jj<cacheconfig.L2setsize ; jj++)
-                            {
-                                if((tag_arrayL2[indexL2][jj]==tagL2) && (valid_arrayL2[indexL2][jj]==1))
-                                   {
-                                       L2AcceState = 1;
-                                       for(int jj =0; jj < cacheconfig.L1setsize;jj++)
-                                       {
-                                           if(valid_arrayL1[indexL1][jj] == 0)
-                                           {
-                                               tag_arrayL1[indexL1][jj] = tagL1;
-                                               valid_arrayL1[indexL1][jj] = 1;
-                                               break;
-                                           }
-                                           else
-                                           {
-                                               jj = countL1;
-                                               tag_arrayL1[indexL1][jj] = tagL1;
-                                               countL1 =countL1 + 1;                //
-                                               if(countL1 == cacheconfig.L1setsize)
-                                               {
-                                                   countL1=0;
-                                               }
-                                           }
-                                       }
-                                      break;
-                                   }
-                            }
-                    if(L2AcceState==0)
-                        {
-                            L2AcceState = 2;
-                            for(int jj = 0;jj < cacheconfig.L2setsize;jj++)
-                            {
+                 
+                 for(int jj = 0 ; jj<cacheconfig.L1setsize;jj++){
+                     if((tag_arrayL1[indexL1][jj] == tagL1) && (valid_arrayL1[indexL1][jj] == 1)){
+                         L2AcceState =0;
+                         L1AcceState =1;
+                         cout<<" Read HIT NA \n";
+                         break;
+                     }
+                 }
+                  if(L1AcceState == 0){
+                      L1AcceState = 2;
+                      for(int jj = 0;jj<cacheconfig.L2setsize ; jj++){
+                          if((tag_arrayL2[indexL2][jj]==tagL2) && (valid_arrayL2[indexL2][jj]==1)){
+                              L2AcceState = 1;
+                               for(int jj =0; jj < cacheconfig.L1setsize;jj++){
+                                  if(valid_arrayL1[indexL1][jj] == 0){
+                                        tag_arrayL1[indexL1][jj] = tagL1;
+                                        valid_arrayL1[indexL1][jj] = 1;
+                                        break;
+                                  }
+                                  else{
+                                        jj = countL1;
+                                        tag_arrayL1[indexL1][jj] = tagL1;
+                                        countL1 =countL1 + 1;              
+                                        
+                                        if(countL1 == cacheconfig.L1setsize){
+                                          countL1=0;
+                                        }
+                                  }
+                               }
+                               break;
+                          }
+                      }
+                      if(L2AcceState == 0){
+                          L2AcceState = 2;
+                          for(int jj = 0;jj < cacheconfig.L2setsize;jj++){
                             
-                                if(valid_arrayL2[indexL2][jj]==0)
-                                {
+                                if(valid_arrayL2[indexL2][jj]==0){
                                     tag_arrayL2[indexL2][jj] = tagL2;
                                     valid_arrayL2[indexL2][jj] = 1;
-                                    //goto w;
                                     break;
                                 }
-                                else
-                                {
+                                else{
                                     jj = countL2;
                                     tag_arrayL2[indexL2][jj] = tagL2;
                                     countL2 = countL2 + 1;
-                                    if(countL2 == cacheconfig.L2setsize)
-                                    {
+                                    if(countL2 == cacheconfig.L2setsize){
                                         countL2 = 0;
                                     }
                                 }
-                            }
-                            for(int jj = 0 ; jj < cacheconfig.L1setsize ; jj ++)
-                            {
-                                if(valid_arrayL1[indexL1][jj] == 0)              //tagarr
-                                {
+                          }
+                          for(int jj = 0 ; jj < cacheconfig.L1setsize ; jj ++){
+                              if(valid_arrayL1[indexL1][jj] == 0){
                                     tag_arrayL1[indexL1][jj] = tagL1;
                                     valid_arrayL1[indexL1][jj] =1;
-                                    break;              //no break;
-                                }
-                                else
-                                {
+                                    break;              
+                              }
+                              else{
                                     jj = countL1;
                                     tag_arrayL1[indexL1][jj] = tagL1;
                                     countL1 += 1;
-                                    if(countL1== cacheconfig.L1setsize)
-                                    {
+                                    if(countL1== cacheconfig.L1setsize){
                                         countL1 = 0;
                                     }
-                                }
-                            }
-                                
-                        }
+                              }
+                          }
+                      }
                                    
-                    }
-                }
+                  }
+            }
                             
-             else 
-             {    
-                   //Implement by you:
-                  // write access to the L1 Cache, 
-                  //and then L2 (if required), 
-                  //update the L1 and L2 access state variable;
-                 
-                 for(int jj = 0 ; jj < cacheconfig.L1setsize ; jj ++)
-                 {
-                  
-                     
-                     if((tag_arrayL1[indexL1][jj] == tagL1) && (valid_arrayL1[indexL1][jj]==1))
-                     {
+            else{    
+                 for(int jj = 0 ; jj < cacheconfig.L1setsize ; jj ++){
+                     if((tag_arrayL1[indexL1][jj] == tagL1) && (valid_arrayL1[indexL1][jj]==1)){
                          L1AcceState = 3;
                          L2AcceState = 0;
                          cout<<"";
                          break;
                      }
-                  }
-                 //
-                 if(L1AcceState == 0)
-                 {
-                 L1AcceState = 4;
-                 for(int jj =0 ; jj < cacheconfig.L2setsize; jj++)
-                 {
-                     if((tag_arrayL2[indexL2][jj] == tagL2)&& (valid_arrayL2[indexL2][jj]==1))
-                     {
-                         L2AcceState = 3;
-                         break;
-                        // break;
-                     }
-                     
                  }
-                   if(L2AcceState==0)
-                     {
-                       L2AcceState =4;
+                 
+                 if(L1AcceState == 0){
+                     L1AcceState = 4;
+                     for(int jj =0 ; jj < cacheconfig.L2setsize; jj++){
+                         if((tag_arrayL2[indexL2][jj] == tagL2)&& (valid_arrayL2[indexL2][jj]==1)){
+                             L2AcceState = 3;
+                             break;
+                         }
+                     
+                     }
+                     if(L2AcceState==0){
+                        L2AcceState = 4;
                         
                      }
-                }
+                 }
             
-             }
-            
-              
-              
-            
-        tracesout<< L1AcceState << " " << L2AcceState << endl;  // Output hit/miss results for L1 and L2 to the output file;
+            }
+            tracesout<< L1AcceState << " " << L2AcceState << endl;  // Output hit/miss results for L1 and L2 to the output file;
             L1AcceState = 0;
             L2AcceState = 0;
              
@@ -349,12 +273,5 @@ int main(int argc, char* argv[]){
         tracesout.close(); 
     }
     else cout<< "Unable to open trace or traceout file ";
-
-
-   
-    
-  
-
-   
     return 0;
 }
